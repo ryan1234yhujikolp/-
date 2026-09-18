38bb09d1ee0cb5e713e08d5748af0b80f8724767
ryan1234yhujikolp <siyoon.com123@kakao.com>
2026-09-18 16:12:48 +0900

    fix: configure base path for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/-",
+  assetPrefix: "/-/",
   images: { unoptimized: true },
 };
 export default nextConfig;
