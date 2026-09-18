17280f6435e3a931cde1d89b953630904c2be95d
ryan1234yhujikolp <siyoon.com123@kakao.com>
2026-09-18 16:14:08 +0900

    fix: prepare static export for Netlify anonymous deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/-",
-  assetPrefix: "/-/",
   images: { unoptimized: true },
 };
 export default nextConfig;
